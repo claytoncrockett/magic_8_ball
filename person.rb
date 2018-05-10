a73074092b79570b6ee99c6094f9a060a77c01c3
Clayton Crockett <claytoncrockett@gmail.com>
2018-05-10 15:35:19 -0600

    changed first conditional to ternary

--- person.rb
+++ person.rb
@@ -6,15 +6,7 @@ class Person
     puts "You find yourself in front of a magic 8 ball"
     puts "Would you like to ask it a question? (Y/N)"
     response = gets.strip
-    case response
-    when "Y"
-      shake_magic8_ball
-    when "N"
-      exit
-    else
-      puts "I'm sorry I did not understand"
-      initialize
-    end
+    (response == "Y") ? shake_magic8_ball : exit
   end
 
   def shake_magic8_ball
